3ee9cb2f5f22f463a233338f1999aeb47fdb72ae
MykolaMyroshnychenko <mykola.myroshnychenko@hneu.net>
2021-04-16 17:27:08 +0300

    Module 4

--- src/myr/vsem/module4/Results4.java
+++ src/myr/vsem/module4/Results4.java
@@ -31,9 +31,9 @@ public class Results4 {
 
         LocalDateTime finish = LocalDateTime.now();
 
-        System.out.println(ChronoUnit.MILLIS.between(start, finish));
+        System.out.println(ChronoUnit.MILLIS.between(start, finish) + " - MILLIS");
 
-        System.out.println("---------------------------------");
+        System.out.println("----------------------------------------------------");
 
         start = LocalDateTime.now();
         for (int i = 0; i < 5 ; i++) {
@@ -41,3 +41,29 @@ public class Results4 {
         }
     }
 }
+/*
+Result:
+3928 - MILLIS
+----------------------------------------------------
+Thread-0 has been started at 2021-04-16T16:52:01.038
+Thread-1 has been started at 2021-04-16T16:52:01.038
+Thread-2 has been started at 2021-04-16T16:52:01.039
+Thread-3 has been started at 2021-04-16T16:52:01.040
+Thread-4 has been started at 2021-04-16T16:52:01.041
+Thread-0 has been finished at 2021-04-16T16:52:01.992
+Thread-0 Duration is 954
+Thread-2 has been finished at 2021-04-16T16:52:02.016
+Thread-2 Duration is 977
+Thread-4 has been finished at 2021-04-16T16:52:02.033
+Thread-3 has been finished at 2021-04-16T16:52:02.033
+Thread-4 Duration is 992
+Thread-3 Duration is 993
+Thread-1 has been finished at 2021-04-16T16:52:02.042
+Thread-1 Duration is 1004
+
+Conclusion:
+On average, each thread is 984 milliseconds.
+The difference between the first and the last thread is 0.050 milliseconds.
+Comparing the two methods, the fastest is multi-thread,
+ using it we significantly save time for information processing.
+ */
